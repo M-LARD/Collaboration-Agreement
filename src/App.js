b28ecf88b7ed5b393dfd967b6a41a3249b8e6413
Lauren Main <lauren.main28@gmail.com>
2022-09-10 15:15:54 -0700

    starts frontend and header footer aboutus pages

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import React from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import { MantineProvider } from '@mantine/core';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <>
+          <Header />
+          <Main/>
+          <Footer />
+        </>
+      </div>
+    );
+  }
+}
+
+export default App;
