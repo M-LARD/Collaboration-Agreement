42d0ec998c6bf1557355df9df8c5341ff24bbfe5
Daniel Brott <danbrott42@gmail.com>
2022-09-13 19:12:47 -0700

    added some css styling

--- src/SavedCities.js
+++ src/SavedCities.js
@@ -1,28 +1,94 @@
 import React from "react";
-
+import { MantineProvider, Box, Text } from "@mantine/core";
 
 class SavedCities extends React.Component {
   render() {
     return (
-      <>
+      <MantineProvider>
         {this.props.savedResults.length ? (
           this.props.savedResults.map((city) => (
-            <div key={city._id}>
-              <p>{city.city}</p>
-              <p>{city.col_idx}</p>
-              <p>{city.rent_index}</p>
-              <p>{city.col_plus_idx}</p>
-              <p>{city.groceries_idx}</p>
-              <p>{city.restaurant_idx}</p>
-              <p>{city.local_purchasing_pwr_idx}</p>
-            </div>
+            <Box
+              key={city._id}
+              sx={{
+                color: "#00ECE5",
+                fontSize: 22,
+                fontWeight: 'bolder',
+                fontFamily: 'sans-serif',
+                border: "4px solid white",
+                lineHeight: 1.4,
+              }}
+            >
+              <Text
+                sx={{
+                  "&:hover": {
+                    backgroundColor: "#eee",
+                  },
+                }}
+              >
+                {city.city}
+              </Text>
+              <Text
+                sx={{
+                  "&:hover": {
+                    backgroundColor: "#eee",
+                  },
+                }}
+              >
+                {city.col_idx}
+              </Text>
+              <Text
+                sx={{
+                  "&:hover": {
+                    backgroundColor: "#eee",
+                  },
+                }}
+              >
+                {city.rent_index}
+              </Text>
+              <Text
+                sx={{
+                  "&:hover": {
+                    backgroundColor: "#eee",
+                  },
+                }}
+              >
+                {city.col_Textlus_idx}
+              </Text>
+              <Text
+                sx={{
+                  "&:hover": {
+                    backgroundColor: "#eee",
+                  },
+                }}
+              >
+                {city.groceries_idx}
+              </Text>
+              <Text
+                sx={{
+                  "&:hover": {
+                    backgroundColor: "#eee",
+                  },
+                }}
+              >
+                {city.restaurant_idx}
+              </Text>
+              <Text
+                sx={{
+                  "&:hover": {
+                    backgroundColor: "#eee",
+                  },
+                }}
+              >
+                {city.local_purchasing_pwr_idx}
+              </Text>
+            </Box>
           ))
         ) : this.props.errorMessage ? (
           <p>{this.props.errorMessage}</p>
         ) : (
           <p>No cities saved</p>
         )}
-      </>
+      </MantineProvider>
     );
   }
 }
