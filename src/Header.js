41c69d09b8cbdd4fd7075e29c9c07aa844684dff
Lauren Main <lauren.main28@gmail.com>
2022-09-15 18:29:19 -0700

    fixes router

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { BrowserRouter as  Link, } from "react-router-dom";
-import { MantineProvider, Navbar, Box, Header, Text } from "@mantine/core";
+import {  Link, } from "react-router-dom";
+import { MantineProvider, Navbar, Box, Header, Anchor } from "@mantine/core";
 // import SavedCities from "./SavedCities";
 // import About from "./About";
 // import Main from "./Main";
@@ -8,7 +8,7 @@ import { MantineProvider, Navbar, Box, Header, Text } from "@mantine/core";
 class Head extends React.Component {
   render() {
     return (
-      // <Router>
+    
         <Box>
           <MantineProvider
             theme={{
@@ -26,25 +26,20 @@ class Head extends React.Component {
               />
               Prices Right
                 
-                  <Text component={Link} varient="link" to="/"style={{paddingRight:'.75rem'}}>
+                  <Anchor component={Link} varient="link" to="/"style={{paddingRight:'.75rem'}}>
                     Home
-                  </Text>
-                  <Text component={Link} varient="link" to="/About"style={{paddingRight:'.75rem'}}>
+                  </Anchor>
+                  <Anchor component={Link} varient="link" to="/About"style={{paddingRight:'.75rem'}}>
                     About Us
-                  </Text>
-                  <Text component={Link} varient="link" to="/SavedCities"style={{paddingRight:'.75rem'}}>
+                  </Anchor>
+                  <Anchor component={Link} varient="link" to="/SavedCities"style={{paddingRight:'.75rem'}}>
                     Saved Cities
-                  </Text>
-                  {/* <Routes>
-                  <Route path= '/' element={<Main/>}/>
-                  <Route path="/About" element={<About />} />
-                  <Route path="/SavedCities" element={<SavedCities />} />
-                </Routes> */}
+                  </Anchor>
+            
                 </Header>
               <Navbar.Section>{/* Footer with user */}</Navbar.Section>
           </MantineProvider>
         </Box>
-      // </Router>
     );
   }
 }
